dfc0285ee79da1ac6ca8eed03b672087c57b4d02
Tobias Kärst <tobias.kaerst@qbeyond.de>
2022-10-19 18:03:12 +0200

    fix: removed image lib for ssr

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
-import image from '@astrojs/image';
 import compress from 'astro-compress';
 
 import { SITE, PAGES } from './src/config';
@@ -12,13 +11,7 @@ export default defineConfig({
   site: SITE.origin,
   base: SITE.basePathname,
 
-  integrations: [
-    sitemap({ customPages: PAGES }),
-    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
-    compress(),
-    tailwind(),
-    preact(),
-  ],
+  integrations: [sitemap({ customPages: PAGES }), compress(), tailwind(), preact()],
 
   output: 'server',
   adapter: node({
